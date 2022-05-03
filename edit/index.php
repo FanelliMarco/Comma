061892dc5230237101b228b23f0d0629d8ec24e2
MaggioUtils <maggiololuca7@gmail.com>
2022-05-03 14:35:41 +0200

    Edit NC Page Basement

---
+++ edit/index.php
@@ -0,0 +1,20 @@
+<?php
+define("TITLE", "Comma - Login");
+include "../assets/layouts/header.php";
+?>
+
+<!-- Non conformità -->
+<section class="p-3 w-100 d-flex align-items-center justify-content-center">
+    <span class="page_subtitle text-uppercase text-light">modifica non conformità</span>
+</section>
+
+<!-- Form container -->
+<section class="p-1">
+    <div class="container">
+
+    </div>
+</section>
+
+<?php
+include "../assets/layouts/footer.php";
+?>

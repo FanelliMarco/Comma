d0e7989d2e3c138bf3d249b3345d7bb4e3d2afda
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-03 22:22:03 +0200

    Update index.php

--- login/includes/login_include.php
+++ login/includes/login_include.php
@@ -1,45 +1,55 @@
 <?php
-    require_once "../../assets/setup/connessionedb.php";
-    include "../../assets/includes/security_functions.php";
-    include "../../assets/includes/query_include.php";
+    if(isset($_POST['sub'])){
+        require_once "../../assets/setup/connessionedb.php";
+        include "../../assets/includes/security_functions.php";
+        include "../../assets/includes/query_include.php";
 
-    //Controla se i campi sono vuoti
-    if(empty($_POST['user']) || empty($_POST['pw'])){
-        $_SESSION['error']='I campi non possono essere vuoti';
-        header("Location: ../");
-        exit();
-    }
-    else{
-        //Controlla se i campi contengono caratteri/parole considerati non validi
-        if(_cleaninjections($_POST['user']) && _cleaninjections($_POST['pw'])){
-            $_SESSION['error']='I campi contengono caratteri o parole non ammesse';
+        //Controla se i campi sono vuoti
+        if(empty($_POST['user']) || empty($_POST['pw'])){
+            $_SESSION['error']='I campi non possono essere vuoti';
             header("Location: ../");
             exit();
         }
         else{
-            $user=htmlspecialchars($_POST['user']);
-            $pw=hash("sha256", htmlspecialchars($_POST['pw']));
+            //Controlla se i campi contengono caratteri/parole considerati non validi
+            if(_cleaninjections($_POST['user']) && _cleaninjections($_POST['pw'])){
+                $_SESSION['error']='I campi contengono caratteri o parole non ammesse';
+                header("Location: ../");
+                exit();
+            }
+            else{
+                $user=htmlspecialchars($_POST['user']);
+                $pw=hash("sha256", htmlspecialchars($_POST['pw']));
 
-            $stmt=$conn->prepare($search_user_employee);
-            $stmt->bind_param("ss", $user, $pw);
-
-            $stmt->execute();
-            
-            //Variabile contente il possibile impiegato
-            $res_e=$stmt->get_result();
-
-            //Controlla se l'impiegato è presente nel database, se non è presente controlla che potrebbe essere un cliente
-            if(!$res){
-                $stmt=$conn->prepare($search_user_client);
+                $stmt=$conn->prepare($search_user_employee);
                 $stmt->bind_param("ss", $user, $pw);
-                $stmt->execute();
-                $res_c=$stmt->get_result();
 
-                //Controlla il caso in cui ci sia un cliente che deve accedere
-                if(!$res_C){
-                    $_SESSION['error']='Utente o password errati';
-                    header("Location: ../");
-                    exit();
+                $stmt->execute();
+                
+                //Variabile contente il possibile impiegato
+                $res_e=$stmt->get_result();
+
+                //Controlla se l'impiegato è presente nel database, se non è presente controlla che potrebbe essere un cliente
+                if(!$res){
+                    $stmt=$conn->prepare($search_user_client);
+                    $stmt->bind_param("ss", $user, $pw);
+                    $stmt->execute();
+                    $res_c=$stmt->get_result();
+
+                    //Controlla il caso in cui ci sia un cliente che deve accedere
+                    if(!$res_C){
+                        $_SESSION['error']='Utente o password errati';
+                        header("Location: ../");
+                        exit();
+                    }
+                    else{
+                        $row=$res->fetch_assoc();
+                        session_start();
+                        $_SESSION['logged']=true;
+                        $_SESSION['user']=$user;
+                        header("Location: ../../management");
+                        exit();
+                    }
                 }
                 else{
                     $row=$res->fetch_assoc();
@@ -49,14 +59,6 @@
                     header("Location: ../../management");
                     exit();
                 }
-            }
-            else{
-                $row=$res->fetch_assoc();
-                session_start();
-                $_SESSION['logged']=true;
-                $_SESSION['user']=$user;
-                header("Location: ../../management");
-                exit();
             }
         }
     }

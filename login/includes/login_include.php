3357abb54c2b65f7a91a0f6364949130fb7c740e
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-04 16:03:39 +0200

    Update script.js

--- login/includes/login_include.php
+++ login/includes/login_include.php
@@ -1,40 +1,47 @@
 <?php
-    include "../../assets/setup/connessionedb.php";
-    include "../../assets/includes/security_functions.php";
-    include "../../assets/includes/query_include.php";
+    session_start();
+    if(isset($_POST['sub'])){
+        require_once "../../assets/setup/connessionedb.php";
+        require "../../assets/includes/security_functions.php";
+        require "../../assets/includes/query_include.php";
 
-    if(isset($_POST['sub']) && !empty($_POST['user']) && !empty($_POST['pw'])){
-        if(controllaCaratteriAmmessi($_POST['user']) && controllaParoleVietate($_POST['user']) && controllaParoleVietate($_POST['pw']) && controllaCaratteriAmmessi($_POST['pw'])){
-            $user=htmlspecialchars($_POST['user']);
-            $pw=hash("sha256", htmlspecialchars($_POST['pw']));
-
-            $stmt=$conn->prepare($search_user_employee);
-            $stmt->bind_param("ss", $user, $pw);
-
-            $stmt->execute();
-
-            $res=$stmt->get_result();
-
-            if($res){
-                $row=$res->fetch_assoc();
-                session_start();
-                $_SESSION['logged']=true;
-                $_SESSION['user']=$user;
-                header("Location: ../../management");
-            }
-            else{
-                //echo $conn->error;
-                //echo "Login fallito";
-                header("Location: ../");
-            }
+        //Controla se i campi sono vuoti
+        if(empty($_POST['user']) || empty($_POST['pw'])){
+            $_SESSION['error']='I campi non possono essere vuoti';
+            header("Location: ../");
+            exit();
         }
         else{
-            //echo "Controllo fallito";
-            header("Location: ../");
+            //Controlla se i campi contengono caratteri/parole considerati non validi
+            if(_cleaninjections($_POST['user']) || _cleaninjections($_POST['pw'])){
+                $_SESSION['error']='I campi contengono caratteri o parole non ammesse';
+                header("Location: ../");
+                exit();
+            }
+            else{
+                $user=htmlspecialchars($_POST['user']);
+                $pw=hash("sha256", htmlspecialchars($_POST['pw']));
+
+                $stmt=$conn->prepare($search_user_employee);
+                $stmt->bind_param("ss", $user, $pw);
+
+                $stmt->execute();
+                
+                //Variabile contente il possibile impiegato
+                $res_e=$stmt->get_result();
+
+                if(!$res_e){ 
+                    $_SESSION['error']='Utente o password errati';
+                    header("Location: ../");
+                    exit();
+                }
+
+                //$row=$res_e->fetch_assoc();
+                $_SESSION['logged']=true;
+                $_SESSION['user']=$user;
+                header("Location: ../../dashboard");
+                exit();
+            }
         }
-    }
-    else{
-        //echo "Qualcosa fallito";
-        //header("Location: ../");
     }
 ?>

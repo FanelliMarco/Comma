47d4884a2c7bab24a0496fc0f01955d5fa81ba85
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-05 17:13:11 +0200

    update

--- report/index.php
+++ report/index.php
@@ -1,5 +1,5 @@
 <?php
-define("TITLE", "Comma - Login");
+define("TITLE", "Comma - Report");
 include "../assets/layouts/header.php";
 ?>
 
@@ -12,36 +12,36 @@ include "../assets/layouts/header.php";
 <!-- Form per compilazione -->
 <section class="p-5">
     <div class="container">
-        <form class="form-auth flex-column text-white" action="" method="POST">
+        <form class="form-auth flex-column text-white" action="./includes/report_include.php" method="POST">
             <!-- BACKEND completare gli id di ogni tag input e del tag button di conferma-->
             <!-- nel campo for dei label copiare il corrispettivo id del campo input associato -->
             <div class="row g-3">
                 <div class="col-lg-6 col-md-12 p-5">
                     <div class="form-group">
-                        <label for="" class="h2 form-label mb-3 ml-3">Processo di origine</label>
-                        <input type="text" class="form-control mb-3 form_control_font" id="">
+                        <label for="processo" class="h2 form-label mb-3 ml-3">Processo di origine</label>
+                        <input type="text" class="form-control mb-3 form_control_font" id="processo" name='processo'>
                     </div>
                     <div class="form-group">
-                        <label for="" class="h2 form-label mb-3 ml-3">
-                            <select class="form-select">
+                        <label for="fase" class="h2 form-label mb-3 ml-3">
+                            <select class="form-select" name='fase'>
                                 <option value="Semilavorato" selected>Prodotto</option>
                                 <option value="Semilavorato">Semilavorato</option>
                                 <option value="Altro">Altro</option>
                             </select>
                         </label>
-                        <input type="text" class="form-control mb-3 form_control_font" id="">
+                        <input type="text" class="form-control mb-3 form_control_font" id="fase" name='descr_fase'>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12 p-5">
                     <div class="form-group">
-                        <label for="" class="h2 form-label mb-3 ml-3">Descrizione </label>
-                        <input type="text" class="form-control mb-3 form_control_font form_control_descrizione" id="">
+                        <label for="descrizione" class="h2 form-label mb-3 ml-3">Descrizione </label>
+                        <input type="text" class="form-control mb-3 form_control_font form_control_descrizione" id="descrizione" name='descrizione'>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col form-group d-flex justify-content-center">
-                    <button type="submit" class="btn btn-primary w-50 text-uppercase" id=""><span class="form_control_font">conferma</span></button>
+                    <button type="submit" class="btn btn-primary w-50 text-uppercase" id="sub" name='sub'><span class="form_control_font">conferma</span></button>
                 </div>
             </div>
         </form>

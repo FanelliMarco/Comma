14b0754d5833001a0db073892c3bf68ddca23b6d
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-05 20:01:42 +0200

    update

--- login/includes/login_include.php
+++ login/includes/login_include.php
@@ -30,6 +30,7 @@
                     $_SESSION['logged'] = true;
                     $_SESSION['user'] = $user;
                     $_SESSION['matricola'] = $result[0]['Matricola'];
+                    $_SESSION['error']='';
                     header("Location: ../../management");
                     exit();
                 }

13497d5a312a6f73533050181c7c806dbaa70c3e
leonardorigon <78793424+leonardorigon@users.noreply.github.com>
2022-05-06 01:13:59 +0200

    Update query_include.php

--- assets/includes/query_include.php
+++ assets/includes/query_include.php
@@ -41,9 +41,9 @@
     if(!defined("update_user_employee")) define("update_user_employee", "UPDATE impiegato SET Nome=?, Cognome=?, Username=?, Password=?, Tipo=?, Processo=?, updated_at=now() WHERE Matricola=?"); // aggiornamento dati dell'impiegato
 
     // ORDINAMENTI NC DASHBOARD
-    if(!defined("order_nc_number")) define("order_nc_all", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=? ORDER BY numero"); //ordinamento per Numero
-    if(!defined("order_nc_data")) define("order_nc_number", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=? ORDER BY data"); //ordinamento per data
-    if(!defined("order_nc_state")) define("order_nc_state", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=? ORDER BY stato"); //ordinamento per stato
-    if(!defined("order_nc_priority")) define("order_nc_priority", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=? ORDER BY priorita"); //ordinamento per priorita
-    if(!defined("order_nc_origin")) define("order_nc_origin", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=? ORDER BY origine"); //ordinamento per origine
+    if(!defined("order_nc_number")) define("order_nc_all", "SELECT * FROM vi_riepilogo WHERE gestore=? ORDER BY numero"); //ordinamento per Numero
+    if(!defined("order_nc_data")) define("order_nc_number", "SELECT * FROM vi_riepilogo WHERE gestore=? ORDER BY data"); //ordinamento per data
+    if(!defined("order_nc_state")) define("order_nc_state", "SELECT * FROM vi_riepilogo WHERE gestore=? ORDER BY stato"); //ordinamento per stato
+    if(!defined("order_nc_priority")) define("order_nc_priority", "SELECT * FROM vi_riepilogo WHERE ORDER BY priorita"); //ordinamento per priorita
+    if(!defined("order_nc_origin")) define("order_nc_origin", "SELECT * FROM vi_riepilogo WHERE gestore=? ORDER BY origine"); //ordinamento per origine
 ?>

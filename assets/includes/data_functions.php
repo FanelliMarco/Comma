2cb7214bb62c74e533c4eb2b98ef4e4998f432c5
PietroBortolami <piotr.borto01@gmail.com>
2022-05-06 01:09:07 +0200

    Update data_functions.php

--- assets/includes/data_functions.php
+++ assets/includes/data_functions.php
@@ -265,6 +265,15 @@
 
         try {
 
+            $result = db_get_nc($numero, $tipo);
+
+            if(!isset($stato))              $stato = $result[0]["stato"];
+            if(!isset($priorita))           $priorita = $result[0]["priorita"];
+            if(!isset($risolutore))         $risolutore = $result[0]["risolutore"];
+            if(!isset($verificatore))       $verificatore = $result[0]["verificatore"];
+            if(!isset($decisioni))          $decisioni = $result[0]["decisioni"];
+            if(!isset($az_corr))            $az_corr = $result[0]["az_corr"];
+
             $stmt->bind_param("ssssssss", $stato, $priorita, $risolutore, $verificatore, $decisoni, $az_corr, $numero, $tipo);
 
             if(!$stmt->execute())

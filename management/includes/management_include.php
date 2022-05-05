c2d99a4b078e7d4c47f85b81e6d45df8fe6a1ebb
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-05 16:13:23 +0200

    Update

--- management/includes/management_include.php
+++ management/includes/management_include.php
@@ -1,5 +1,24 @@
 <?php
-    require "../assets/includes/data_functions.php";
+    require "../../assets/includes/data_functions.php";
 
     session_start();
+
+    if(isset($_POST['search_button'])){
+        if(!(empty($_POST['search_field']))){
+            if(_cleaninjections($_POST['search_field'])){
+                $_SESSION['error']='Parole non ammesse';
+                header("Location: ../");
+                exit();
+            }
+            else{
+                $search_field=$_POST['search_field'];
+                $result=fill_NC_table_search($search_field);
+            }
+        }
+        else{
+            $_SESSION['error']='Inserire qualcosa da cercare';
+            header("Location: ../");
+            exit();
+        }
+    }
 ?>

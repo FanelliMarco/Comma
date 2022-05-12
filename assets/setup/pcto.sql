2c1abd4f37b4cfe0ab594ed9b15858745d565789
PietroBortolami <102406360+PietroBortolami@users.noreply.github.com>
2022-05-12 21:59:38 +0200

    Update pcto.sql

--- assets/setup/pcto.sql
+++ assets/setup/pcto.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4deb2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Creato il: Mag 12, 2022 alle 17:20
--- Versione del server: 10.5.15-MariaDB-0+deb11u1
--- Versione PHP: 7.4.28
+-- Host: 127.0.0.1
+-- Creato il: Mag 12, 2022 alle 19:13
+-- Versione del server: 10.4.14-MariaDB
+-- Versione PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -108,40 +108,39 @@ CREATE TABLE `impiegato` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `deleted_at` timestamp NULL DEFAULT NULL,
-  `last_login_at` timestamp NULL DEFAULT NULL,
-  `email` varchar(60) NOT NULL,
-  `licenziato` tinyint(1) DEFAULT NULL
+  `last_login_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `impiegato`
 --
 
-INSERT INTO `impiegato` (`Matricola`, `Nome`, `Cognome`, `Username`, `Password`, `Tipo`, `Processo`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`, `email`, `licenziato`) VALUES
-('0000001', 'Giuseppe', 'Manni', 'giuseppe_manni', 'EFF0E93D9B1B8DE86F48B88C261A222BCE4F65B6E3ED19A12124913F34B38951', 'Operaio', 'controllo qualita input', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000002', 'Pietro', 'Di Angelo', 'pietro_angelo', '193CA2DCA9B38D230B00E3E6F90C080D19E15EA564C3D0A2F64C8918CDF822ED', 'Operaio', 'controllo qualita input', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000003', 'Laura', 'Romani', 'laura_romani', 'CFD715DB9F65CA8CE14184BC20A5F51BED592C5D5DC951032FFF65C220983D3E', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000004', 'Rodrigo', 'Costa', 'rodrigo_costa', 'F69F8FC89B7548FF0203C39470AD61CAEF36232BB11A924DE1385FC17E5B625F', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000005', 'Luigi', 'De Cicco', 'luigi_ciccio', '2543CDA7BFA2FC4C1297E91DA053A15316128B88BDCDFF9747E3C78530B1D919', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000006', 'Francesco', 'Padovan', 'francesco_padovan', 'BAE1474BE5ED291846D58B3638A8370532B5A4E7D2CA81E751D7119EF4F18E59', 'Operaio', 'stampaggio grafica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000007', 'Angelo', 'Piazza', 'angelo_piazza', '712E983D1F9103D34238A6347BC34CCBC49A2E4CD5EFC614C83B356987755651', 'Operaio', 'stampaggio grafica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000008', 'Maria', 'Baglio', 'maria_baglio', 'A581A7E95280E9BFB137D969D8C47C2FC7B854747A842C2BA3EC2C6B0E78D20E', 'Operaio', 'posizionamento degli elaborati', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000009', 'Anna', 'Cremonesi', 'anna_cremonesi', '1DAA07252B565FED430AB696519472494AA53F5C8127DFCC42BE17D81F1403E1', 'Operaio', 'inserimento contactless', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000010', 'Giuseppina', 'Toscani', 'giuseppina_toscani', '6D9D8FE9A441F78FD8EB2E285586FAAF54C8E11A8174BA0C53A3C0E1A62F2AC1', 'Operaio', 'laminazione', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000011', 'Angela', 'Longo', 'angela_longo', 'C91FC5AB927FA766B951967F37A00D13A7C461C224DFEA9B8328FF20F411AF82', 'Operaio', 'separazione carte', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000012', 'Concetta', ' Esposito', 'concetta_esposito', '7C51A9804E2DAAD41D5DE074D859441CC36048D4071D93023A7DCDF3FF786529', 'Operaio', 'separazione carte', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000013', 'Giorgio', 'Carraro', 'giorgio_carraro', '25E32A62A67556E6B381364D020C9344DD8AABA12A1D12A3C2D047C6512F1713', 'Operaio', 'applicazione ologramma anticontraffazione', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000014', 'Paolo', 'Fox', 'paolo_fox', '0D06887126CABE32A794727C847A169E5D9962D816AA919401947D7E48704CEC', 'Operaio', 'inserimento chip', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000015', 'Fabio', 'Martinelli', 'fabio_martinelli', '1DACD9F335D148B5CCE7AAB4F2AA8CADE7661C50E2253062B3BEF191A0782264', 'Operaio', 'inserimento chip', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000016', 'Francesca', 'Giordano', 'francesca_giordano', 'E6152480CFEEF866BB84503ED33E828F9D674D1F49A1DBA796D7B440CB5BADED', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000017', 'Matteo', 'Lombardi', 'matteo_lombardi', '551607A1B81F7E6F964B01E57BA5BB0F1E5DB2E374CB63F9DAED2805407A58A3', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000018', 'Carlo', 'Barbieri', 'carlo_barbieri', 'A6D09E4F725406CB9D6010E83B39B708AB099853BABB751C0B0840F8ACF3AA2B', 'Operaio', 'stampa codice carta', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000019', 'Cinzia', 'Moretti', 'cinzia_moretti', 'F8FBC7E94524BE8CC9A6F1707749CBCB59E4826F1455612504183755910E9A0E', 'Operaio', 'logistica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000020', 'Leonardo', 'Rinaldi', 'leonardo_rinaldi', 'D620D07C8743FDABC5995F1ED749BE21EA59E9B41FAE985631BB5F190AB0B67B', 'Operaio', 'logistica', NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000021', 'Basilio', 'Celentano', 'basilio_celentano', '96C9811127209B5DD23C5B1EC7EC69E23074C478BCE3FC99945C1575BF0A94F0', 'Addetto al controllo qualita', NULL, NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000022', 'Fabiola', 'Denari', 'fabiola_denari', '0CB7E0D4D7A397614554381E0516BA74FD5F870A7056C5DD4E518CC6052F1E7A', 'Addetto al controllo qualita', NULL, NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('0000023', 'Igor', 'Rampa', 'igor_rampa', '9755FEFC878A2990AD5123634FD9656E90557875350269EE095643E67BBFD481', 'Addetto al controllo qualita', NULL, NULL, '2022-05-12 15:19:42', NULL, NULL, '', 0),
-('1000000', 'Admin', 'Admin', 'admin', '6B9062CDD10C260A6C092A953E9677CE02C4765BDBCFF9EA77C45561AE11B8C3', 'Boss del database', NULL, '2022-05-05 18:13:23', '2022-05-12 15:19:42', NULL, NULL, '', 0);
+INSERT INTO `impiegato` (`Matricola`, `Nome`, `Cognome`, `Username`, `Password`, `Tipo`, `Processo`, `created_at`, `updated_at`, `deleted_at`, `last_login_at`) VALUES
+('0000001', 'Giuseppe', 'Manni', 'giuseppe_manni', 'EFF0E93D9B1B8DE86F48B88C261A222BCE4F65B6E3ED19A12124913F34B38951', 'Operaio', 'controllo qualita input', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000002', 'Pietro', 'Di Angelo', 'pietro_angelo', '193CA2DCA9B38D230B00E3E6F90C080D19E15EA564C3D0A2F64C8918CDF822ED', 'Operaio', 'controllo qualita input', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000003', 'Laura', 'Romani', 'laura_romani', 'CFD715DB9F65CA8CE14184BC20A5F51BED592C5D5DC951032FFF65C220983D3E', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000004', 'Rodrigo', 'Costa', 'rodrigo_costa', 'F69F8FC89B7548FF0203C39470AD61CAEF36232BB11A924DE1385FC17E5B625F', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000005', 'Luigi', 'De Cicco', 'luigi_ciccio', '2543CDA7BFA2FC4C1297E91DA053A15316128B88BDCDFF9747E3C78530B1D919', 'Operaio', 'irrigidimento plastica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000006', 'Francesco', 'Padovan', 'francesco_padovan', 'BAE1474BE5ED291846D58B3638A8370532B5A4E7D2CA81E751D7119EF4F18E59', 'Operaio', 'stampaggio grafica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000007', 'Angelo', 'Piazza', 'angelo_piazza', '712E983D1F9103D34238A6347BC34CCBC49A2E4CD5EFC614C83B356987755651', 'Operaio', 'stampaggio grafica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000008', 'Maria', 'Baglio', 'maria_baglio', 'A581A7E95280E9BFB137D969D8C47C2FC7B854747A842C2BA3EC2C6B0E78D20E', 'Operaio', 'posizionamento degli elaborati', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000009', 'Anna', 'Cremonesi', 'anna_cremonesi', '1DAA07252B565FED430AB696519472494AA53F5C8127DFCC42BE17D81F1403E1', 'Operaio', 'inserimento contactless', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000010', 'Giuseppina', 'Toscani', 'giuseppina_toscani', '6D9D8FE9A441F78FD8EB2E285586FAAF54C8E11A8174BA0C53A3C0E1A62F2AC1', 'Operaio', 'laminazione', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000011', 'Angela', 'Longo', 'angela_longo', 'C91FC5AB927FA766B951967F37A00D13A7C461C224DFEA9B8328FF20F411AF82', 'Operaio', 'separazione carte', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000012', 'Concetta', ' Esposito', 'concetta_esposito', '7C51A9804E2DAAD41D5DE074D859441CC36048D4071D93023A7DCDF3FF786529', 'Operaio', 'separazione carte', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000013', 'Giorgio', 'Carraro', 'giorgio_carraro', '25E32A62A67556E6B381364D020C9344DD8AABA12A1D12A3C2D047C6512F1713', 'Operaio', 'applicazione ologramma anticontraffazione', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000014', 'Paolo', 'Fox', 'paolo_fox', '0D06887126CABE32A794727C847A169E5D9962D816AA919401947D7E48704CEC', 'Operaio', 'inserimento chip', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000015', 'Fabio', 'Martinelli', 'fabio_martinelli', '1DACD9F335D148B5CCE7AAB4F2AA8CADE7661C50E2253062B3BEF191A0782264', 'Operaio', 'inserimento chip', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000016', 'Francesca', 'Giordano', 'francesca_giordano', 'E6152480CFEEF866BB84503ED33E828F9D674D1F49A1DBA796D7B440CB5BADED', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000017', 'Matteo', 'Lombardi', 'matteo_lombardi', '551607A1B81F7E6F964B01E57BA5BB0F1E5DB2E374CB63F9DAED2805407A58A3', 'Operaio', 'inserimento banda magnetica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000018', 'Carlo', 'Barbieri', 'carlo_barbieri', 'A6D09E4F725406CB9D6010E83B39B708AB099853BABB751C0B0840F8ACF3AA2B', 'Operaio', 'stampa codice carta', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000019', 'Cinzia', 'Moretti', 'cinzia_moretti', 'F8FBC7E94524BE8CC9A6F1707749CBCB59E4826F1455612504183755910E9A0E', 'Operaio', 'logistica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000020', 'Leonardo', 'Rinaldi', 'leonardo_rinaldi', 'D620D07C8743FDABC5995F1ED749BE21EA59E9B41FAE985631BB5F190AB0B67B', 'Operaio', 'logistica', NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000021', 'Basilio', 'Celentano', 'basilio_celentano', '96C9811127209B5DD23C5B1EC7EC69E23074C478BCE3FC99945C1575BF0A94F0', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000022', 'Fabiola', 'Denari', 'fabiola_denari', '0CB7E0D4D7A397614554381E0516BA74FD5F870A7056C5DD4E518CC6052F1E7A', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000023', 'Igor', 'Rampa', 'igor_rampa', '9755FEFC878A2990AD5123634FD9656E90557875350269EE095643E67BBFD481', 'Addetto al controllo qualita', NULL, NULL, '2022-05-02 19:02:31', NULL, NULL),
+('0000024', 'Samuele', 'Bettio', 'samuele_bettio', 'cb0ec2a4f82fae20af3792758524df55845f5103bf8b4742b301162ba27ea8ed', 'Addetto al controllo qualita', NULL, '2022-05-12 14:00:08', '2022-05-12 16:43:09', NULL, '2022-05-12 16:43:09'),
+('1000000', 'Admin', 'Admin', 'admin', '6B9062CDD10C260A6C092A953E9677CE02C4765BDBCFF9EA77C45561AE11B8C3', 'Boss del database', NULL, '2022-05-05 18:13:23', '2022-05-12 17:12:42', NULL, '2022-05-12 17:12:42');
 
 -- --------------------------------------------------------
 
@@ -526,7 +525,7 @@ CREATE TABLE `vi_riepilogo` (
 --
 DROP TABLE IF EXISTS `vi_riepilogo`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`admin`@`localhost` SQL SECURITY DEFINER VIEW `vi_riepilogo`  AS SELECT `nci`.`Numero` AS `numero`, `nci`.`Stato` AS `stato`, `nci`.`Priorita` AS `priorita`, `nci`.`Origine` AS `origine`, `nci`.`Descrizione` AS `descrizione`, `nci`.`Decisioni` AS `decisioni`, `nci`.`Azioni_correttive` AS `az_corr`, `nci`.`Addetto_gestione` AS `gestore`, 'input' AS `tipo`, `rili`.`Data` AS `data`, `rili`.`Materia_prima` AS `oggetto`, `rili`.`Impiegato` AS `segnalatore`, `risi`.`Fornitore` AS `risolutore`, `risi`.`Data_inizio` AS `data_inizio_risoluzione`, `risi`.`Data_fine` AS `data_fine_risoluzione`, `veri`.`Impiegato` AS `verificatore`, `veri`.`Data_inizio` AS `data_inizio_verifica`, `veri`.`Data_fine` AS `data_fine_verifica` FROM (((`nc_input` `nci` left join `rilevamento_input` `rili` on(`nci`.`Numero` = `rili`.`NC`)) left join `risoluzione_input` `risi` on(`nci`.`Numero` = `risi`.`NC`)) left join `verifica_input` `veri` on(`nci`.`Numero` = `veri`.`NC`)) ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vi_riepilogo`  AS SELECT `nci`.`Numero` AS `numero`, `nci`.`Stato` AS `stato`, `nci`.`Priorita` AS `priorita`, `nci`.`Origine` AS `origine`, `nci`.`Descrizione` AS `descrizione`, `nci`.`Decisioni` AS `decisioni`, `nci`.`Azioni_correttive` AS `az_corr`, `nci`.`Addetto_gestione` AS `gestore`, 'input' AS `tipo`, `rili`.`Data` AS `data`, `rili`.`Materia_prima` AS `oggetto`, `rili`.`Impiegato` AS `segnalatore`, `risi`.`Fornitore` AS `risolutore`, `risi`.`Data_inizio` AS `data_inizio_risoluzione`, `risi`.`Data_fine` AS `data_fine_risoluzione`, `veri`.`Impiegato` AS `verificatore`, `veri`.`Data_inizio` AS `data_inizio_verifica`, `veri`.`Data_fine` AS `data_fine_verifica` FROM (((`nc_input` `nci` left join `rilevamento_input` `rili` on(`nci`.`Numero` = `rili`.`NC`)) left join `risoluzione_input` `risi` on(`nci`.`Numero` = `risi`.`NC`)) left join `verifica_input` `veri` on(`nci`.`Numero` = `veri`.`NC`)) ;
 
 --
 -- Indici per le tabelle scaricate

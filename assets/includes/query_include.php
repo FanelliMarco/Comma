9d2dd0b35bd5bb13a455a63d28e433195678c489
marchesaniriccardo <102411974+marchesaniriccardo@users.noreply.github.com>
2022-05-12 15:27:35 +0200

    update query_include

--- assets/includes/query_include.php
+++ assets/includes/query_include.php
@@ -7,7 +7,7 @@
     // DASHBOARD
     if(!defined("search_nc_all")) define("search_nc_all", "SELECT * FROM vi_riepilogo WHERE gestore=? or segnalatore=? or risolutore=? or verificatore=?"); // non conformità relative ad un utente
     if(!defined("view_nc_all")) define("view_nc_all", "SELECT * FROM vi_riepilogo"); // visione totale delle non conformita per l'admin
-    if(!defined("search_nc_data")) define("search_nc_data", "SELECT * FROM vi_riepilogo WHERE gestore=? and data_nc=?"); //non conformita relativa ad un utente e data
+    if(!defined("search_nc_data")) define("search_nc_data", "SELECT * FROM vi_riepilogo WHERE gestore=? and data=?"); //non conformita relativa ad un utente e data
     if(!defined("search_nc_stato")) define("search_nc_stato", "SELECT * FROM vi_riepilogo WHERE gestore=? and stato=?"); //non conformita relativa ad un utente e stato
     if(!defined("search_nc_priorita")) define("search_nc_priorita", "SELECT * FROM vi_riepilogo WHERE gestore=? and priorita=?"); //non conformita relativa ad un utente e priorita
     if(!defined("search_nc_origine")) define("search_nc_origine", "SELECT * FROM vi_riepilogo WHERE gestore=? and origine=?"); //non conformita relativa ad un utente e origine
